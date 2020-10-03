4b916d795907e61f7e0145f95870df7593c82569
igorhenriqueml <igorhenriqueml@gmail.com>
2020-10-02 22:45:59 -0300

    inconformado

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const app = express();
 
 app.use(cors());
 
+app.use(express.json());
+
 // Utiliza as rotas no servidor, conforme importado do arquivo
 app.use(routes);
 
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 import {Router} from "express";
-import HomeController from "./app/HomeController";
+import HomeController from "./app/Controllers/HomeController";
+import UserController from "./app/Controllers/UserController";
 
 const routes = new Router();
 
 routes.get("/", HomeController.index);
+
+routes.get("/users", UserController.index);
+routes.post("/users", UserController.store);
+routes.put("/users/:id", UserController.update);
+routes.delete("/users/:id", UserController.destroy);
+
 
 export default routes;
