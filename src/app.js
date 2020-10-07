8544dc9d3d22b32b6952c8babda25560348d7014
igorhenriqueml <igorhenriqueml@gmail.com>
2020-10-06 22:26:49 -0300

    template_ejs

--- src/app.js
+++ src/app.js
@@ -2,11 +2,28 @@ import express from "express";
 import cors from "cors";
 import routes from "./routes.js";
 import "./database/index"
+import path from "path";
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+/*
+app.use("/", express.static(path.join(__dirname,'public')));
+app.set("views", path.join(__dirname,'src','app','views'));
+app.engine("html", require('ejs').renderFile);
+*/
+
+app.set('view engine', 'ejs');
+app.get("/teste",(req, res) => {
+
+    res.render(path.join(__dirname,'app','views','teste'));
+
+})
+
+
 
 app.use(routes);
 
